d8b15a58c3a7a83bb90018c72cb2f160a4215c91
Nicola Benaglia <nbenaglia@cuebiq.com>
2019-08-27 16:06:55 +0200

    local client (user pc)

---
+++ examples/local-client/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	kubeconfig = flag.String("kubeconfig", "~/.kube/config", "kubeconfig file")
+	flag.Parse()
+	
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+    fmt.Printf("The kubeconfig cannot be loaded: %v\n", err
+    os.Exit(1)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+
+	pod, err := clientset.CoreV1().Pods("my-namespace").Get("my-pod", metav1.GetOptions{})
+}
