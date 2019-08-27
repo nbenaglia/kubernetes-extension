f0799ce75173aa52ff32f438b8a756c795606bbb
Nicola Benaglia <nico.benaz@gmail.com>
2019-08-27 17:33:57 +0200

    Print pod name

--- examples/local-client/main.go
+++ examples/local-client/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -9,15 +11,19 @@ import (
 )
 
 func main() {
-	kubeconfig = flag.String("kubeconfig", "~/.kube/config", "kubeconfig file")
+	kubeconfig = flag.String("kubeconfig", "/Users/nbenaglia/.kube/config", "kubeconfig file")
 	flag.Parse()
-	
+
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
 	if err != nil {
-    fmt.Printf("The kubeconfig cannot be loaded: %v\n", err
-    os.Exit(1)
+		fmt.Printf("The kubeconfig cannot be loaded: %v\n", err)
+		os.Exit(1)
 	}
+
 	clientset, err := kubernetes.NewForConfig(config)
 
 	pod, err := clientset.CoreV1().Pods("my-namespace").Get("my-pod", metav1.GetOptions{})
+
+	fmt.Println(pod.GetName())
 }
